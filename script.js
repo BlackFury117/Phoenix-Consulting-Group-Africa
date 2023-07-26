e54227760efff9841d5297a1af60a95673f1a658
BlackFury117 <idrissadiallo97@icloud.com>
2023-07-26 09:59:05 +0000

    Polishing the code & adding some scrolling effect

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ let countElement4 = document.getElementById("count4");
       }
     }
 }, 10);
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
