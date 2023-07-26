9b67d22b922f7e3b514cf3804d2cb1c00106973b
BlackFury117 <idrissadiallo97@icloud.com>
2023-07-26 10:47:36 +0000

    Removing the scroll animation

--- script.js
+++ script.js
@@ -50,22 +50,22 @@ let countElement4 = document.getElementById("count4");
     }
 }, 10);
 
-const boxes = document.querySelectorAll(".box");
+// const boxes = document.querySelectorAll(".box");
 
-window.addEventListener("scroll", checkBoxes);
+// window.addEventListener("scroll", checkBoxes);
 
-checkBoxes();
+// checkBoxes();
 
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+// function checkBoxes() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
 
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+//   boxes.forEach((box) => {
+//     const boxTop = box.getBoundingClientRect().top;
 
-    if (boxTop < triggerBottom) {
-      box.classList.add("show");
-    } else {
-      box.classList.remove("show");
-    }
-  });
-}
+//     if (boxTop < triggerBottom) {
+//       box.classList.add("show");
+//     } else {
+//       box.classList.remove("show");
+//     }
+//   });
+// }
